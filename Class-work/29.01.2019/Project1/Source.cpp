449d3e0fba60e0a4f6c0dd2a04e33a9985fd2002
павлюк назар <hasab.k18@gmail.com>
2019-01-29 14:06:46 +0200

    2

--- Class-work/29.01.2019/Project1/Source.cpp
+++ Class-work/29.01.2019/Project1/Source.cpp
@@ -1,42 +1,72 @@
-#include <iostream>
-#include <ctime>
-using namespace std;
-
-int foo(int hours = 0, int minutes = 0, int second = 0)
-{
-	hours *= 60;
-	minutes += hours;
-	minutes *=60;
-	minutes += second;
-	return minutes;
-	
-
-}
-int main()
-{
-	srand(unsigned(time(NULL)));
-
-	int hours = 0, minutes = 0, seconds = 0,sum=0;
-
-	/*
-	1.  C++ Написати функцію, яка приймає три аргументи: години, хвилини, секунди. Функція повинна повертати еквівалент переданого їй тимчасового значення в секундах 
-	*/
-
-	cout << "enter hours\n::";
-	cin >>hours;
-	cout << "enter minutes\n::";
-	cin >> minutes;
-	cout << "enter seconds\n::";
-	cin >> seconds;
-
-	sum = foo(hours, minutes, seconds);
-	cout << sum;
+////1.  Написать функцию Dohod, которая вычисляет доход по вкладу.Исходными данными для функции являются : величина вклада, процентная ставка(годовых) и срок вклада(количество дней).
+//#include <iostream>
+//#include <ctime>
+//using namespace std;
+//
+//int foo(int money = 0, int procent = 0, int day = 0)
+//{
+//	return money * procent*day / 365 * 100;
+//	
+//
+//}
+//int main()
+//{
+//	srand(unsigned(time(NULL)));
+//	int money = 0,procent=0,day=0;
+//	cout << "enter money\n::";
+//	cin >> money;
+//	cout << "enter procent\n::";
+//	cin >> procent;
+//	cout << "enter termofdeposit\n::";
+//	cin >> day;
+//	cout << "your money:: "<< foo(money,procent,day);
+//
+//
+//	system("pause");
+//	return 0;
+//}
 
 
 
-
-
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <ctime>
+//using namespace std;
+//
+//int foo(int hours = 0, int minutes = 0, int second = 0)
+//{
+//	hours *= 60;
+//	minutes += hours;
+//	minutes *=60;
+//	minutes += second;
+//	return minutes;
+//	
+//
+//}
+//int main()
+//{
+//	srand(unsigned(time(NULL)));
+//
+//	int hours = 0, minutes = 0, seconds = 0,sum=0;
+//
+//	/*
+//	1.  C++ Написати функцію, яка приймає три аргументи: години, хвилини, секунди. Функція повинна повертати еквівалент переданого їй тимчасового значення в секундах 
+//	*/
+//
+//	cout << "enter hours\n::";
+//	cin >>hours;
+//	cout << "enter minutes\n::";
+//	cin >> minutes;
+//	cout << "enter seconds\n::";
+//	cin >> seconds;
+//
+//	sum = foo(hours, minutes, seconds);
+//	cout << sum;
+//
+//
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
