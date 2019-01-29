e43d4b0470eafef5e2e883488b9a9d808c21e408
павлюк назар <hasab.k18@gmail.com>
2019-01-29 13:10:45 +0200

    push class work

--- Class-work/29.01.2019/Project1/Source.cpp
+++ Class-work/29.01.2019/Project1/Source.cpp
@@ -2,15 +2,35 @@
 #include <ctime>
 using namespace std;
 
+int foo(int hours = 0, int minutes = 0, int second = 0)
+{
+	hours *= 60;
+	minutes += hours;
+	minutes *=60;
+	minutes += second;
+	return minutes;
+	
+
+}
 int main()
 {
 	srand(unsigned(time(NULL)));
 
+	int hours = 0, minutes = 0, seconds = 0,sum=0;
 
+	/*
+	1.  C++ Написати функцію, яка приймає три аргументи: години, хвилини, секунди. Функція повинна повертати еквівалент переданого їй тимчасового значення в секундах 
+	*/
 
+	cout << "enter hours\n::";
+	cin >>hours;
+	cout << "enter minutes\n::";
+	cin >> minutes;
+	cout << "enter seconds\n::";
+	cin >> seconds;
 
-
-
+	sum = foo(hours, minutes, seconds);
+	cout << sum;
 
 
 
