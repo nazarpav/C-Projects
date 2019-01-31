b8131f572e8524fb6021492a1d97089d1c10e152
павлюк назар <hasab.k18@gmail.com>
2019-01-31 12:36:47 +0200

    31.01.2019

---
+++ Class-work/31.01.2019/31.01.2019/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+template <typename T>
+void foo(T a=0)
+{
+
+}
+
+int main()
+{
+	srand(unsigned(time(NULL)));
+
+	
+
+	system("pause");
+	return 0;
+}
+
